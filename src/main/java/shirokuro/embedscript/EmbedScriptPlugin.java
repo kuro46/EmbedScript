b35291d19727478e9c2f1fa574470cbb5f27aeeb
shirokuro <46kuro212@gmail.com>
2018-11-19 18:49:16 +0900

    Initial commit

---
+++ src/main/java/shirokuro/embedscript/EmbedScriptPlugin.java
@@ -0,0 +1,12 @@
+package shirokuro.embedscript;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author shirokuro
+ */
+class EmbedScriptPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
