8fc32da4cfcdf991fe72b35734b7a212d01ffdad
shirokuro <46kuro212@gmail.com>
2018-11-23 02:05:41 +0900

    Add teleport command.

--- src/main/java/shirokuro/embedscript/command/MainCommandExecutor.java
+++ src/main/java/shirokuro/embedscript/command/MainCommandExecutor.java
@@ -1,8 +1,13 @@
 package shirokuro.embedscript.command;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import shirokuro.embedscript.Prefix;
 
 import java.util.Locale;
 
@@ -12,21 +17,40 @@ import java.util.Locale;
 public class MainCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        String args0 = args[0].toLowerCase(Locale.ENGLISH);
-        switch (args0) {
-            case "teleport": {
-                //TODO:
-                /*
-                args1 = player
-                args2 = world
-                args3 = x
-                args4 = y
-                args5 = z
-                 */
-                break;
-            }
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Can't perform this command from the console.");
+            return true;
         }
-        sender.sendMessage("Nothing to here :)");
-        return true;
+        if (args.length < 1) {
+            return false;
+        }
+        Player player = (Player) sender;
+        switch (args[0].toLowerCase(Locale.ENGLISH)) {
+            case "teleport": {
+                if (args.length < 5) {
+                    return false;
+                }
+                World world = Bukkit.getWorld(args[1]);
+                if (world == null) {
+                    player.sendMessage(Prefix.ERROR_PREFIX + "World: " + args[1] + " not exist.");
+                    return true;
+                }
+                try {
+                    player.teleport(new Location(world,
+                        Double.parseDouble(args[2]),
+                        Double.parseDouble(args[3]),
+                        Double.parseDouble(args[4])));
+                } catch (NumberFormatException e) {
+                    player.sendMessage("X or Y or Z is not valid number.");
+                }
+                player.sendMessage(Prefix.SUCCESS_PREFIX + "Teleported.");
+                return true;
+            }
+            case "migrate": {
+                return true;
+            }
+            default:
+                return false;
+        }
     }
 }
