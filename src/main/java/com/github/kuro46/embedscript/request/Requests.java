5b6dca3e389f205955b16bd331e6b53f9a7ee10a
shirokuro <46kuro212@gmail.com>
2019-02-22 23:21:04 +0900

    refactor: remove unnecessary implement in Requests.java

--- src/main/java/com/github/kuro46/embedscript/request/Requests.java
+++ src/main/java/com/github/kuro46/embedscript/request/Requests.java
@@ -3,7 +3,6 @@ package com.github.kuro46.embedscript.request;
 import com.github.kuro46.embedscript.script.ScriptPosition;
 import com.github.kuro46.embedscript.script.ScriptUI;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 
 import java.util.Map;
 import java.util.WeakHashMap;
@@ -11,7 +10,7 @@ import java.util.WeakHashMap;
 /**
  * @author shirokuro
  */
-public class Requests implements Listener {
+public class Requests {
     private final Map<Player, Request> requests = new WeakHashMap<>(2);
     private final ScriptUI scriptUI;
 
