349c988b4cc646f3896bdf9cd60867a8f3830f7d
shirokuro <46kuro212@gmail.com>
2019-03-01 16:55:54 +0900

    feat: throws exception when empty value not allowed

--- src/main/java/com/github/kuro46/embedscript/script/parser/AbstractProcessor.java
+++ src/main/java/com/github/kuro46/embedscript/script/parser/AbstractProcessor.java
@@ -21,6 +21,10 @@ public abstract class AbstractProcessor implements Processor{
         this(null,null);
     }
 
+    public boolean allowEmptyList(){
+        return false;
+    }
+
     public String getKey(){
         return Objects.requireNonNull(key,"key of AbstractProcessor is null!");
     }
@@ -52,6 +56,9 @@ public abstract class AbstractProcessor implements Processor{
         for (Map.Entry<String, List<String>> entry : source.unmodifiableMap().entrySet()) {
             String key = entry.getKey();
             List<String> values = entry.getValue();
+            if (!allowEmptyList() && values.isEmpty()){
+                throw new ParseException("Cannot set empty value to '" + key + "'.");
+            }
             if (key.equals(getKey())) {
                 setup(source, key, values);
             }
