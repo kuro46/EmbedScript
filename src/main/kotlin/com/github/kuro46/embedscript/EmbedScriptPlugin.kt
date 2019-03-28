af3b3a658b276e4dcc8c32bf0a7ca6de4d5cdbc5
shirokuro <46kuro212@gmail.com>
2019-03-28 11:41:06 +0900

    refactor: refactor EmbedScriptPlugin#onEnable

--- src/main/kotlin/com/github/kuro46/embedscript/EmbedScriptPlugin.kt
+++ src/main/kotlin/com/github/kuro46/embedscript/EmbedScriptPlugin.kt
@@ -13,24 +13,15 @@ import java.util.logging.Level
 class EmbedScriptPlugin : JavaPlugin() {
     override fun onEnable() {
         val begin = System.currentTimeMillis()
-        try {
-            EmbedScript.initialize(this)
-        } catch (e: IOException) {
-            logger.log(Level.SEVERE, "Failed to initialize the plugin! disabling...", e)
-            Bukkit.getPluginManager().disablePlugin(this)
-            return
-        } catch (e: InvalidConfigurationException) {
-            logger.log(Level.SEVERE, "Failed to initialize the plugin! disabling...", e)
-            Bukkit.getPluginManager().disablePlugin(this)
-            return
-        } catch (e: IllegalStateException) {
-            logger.log(Level.SEVERE, "Failed to initialize the plugin! disabling...", e)
-            Bukkit.getPluginManager().disablePlugin(this)
-            return
-        }
 
-        val end = System.currentTimeMillis()
-        logger.info("Enabled! (${end - begin}ms)")
+        val initResult = runCatching { EmbedScript.initialize(this) }
+        initResult.exceptionOrNull()?.let {
+            logger.log(Level.SEVERE, "Failed to initialize the plugin! disabling...", it)
+            Bukkit.getPluginManager().disablePlugin(this)
+        } ?: run {
+            val end = System.currentTimeMillis()
+            logger.info("Enabled! (${end - begin}ms)")
+        }
     }
 
     override fun onDisable() {
