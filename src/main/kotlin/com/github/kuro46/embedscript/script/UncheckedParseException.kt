9ab934337343101e6356e086e72e1a37a1ce3362
shirokuro <46kuro212@gmail.com>
2019-03-28 09:45:53 +0900

    refactor: remove unnecessary Exception class

--- src/main/kotlin/com/github/kuro46/embedscript/script/UncheckedParseException.kt
+++
@@ -1,8 +0,0 @@
-package com.github.kuro46.embedscript.script
-
-class UncheckedParseException(cause: ParseException) : RuntimeException(cause) {
-    @Synchronized
-    fun getCause(): ParseException {
-        return super.cause as ParseException
-    }
-}
