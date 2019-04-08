04124df001c462e28698e434c860770370bb4a38
shirokuro <46kuro212@gmail.com>
2019-04-08 21:30:57 +0900

    feat: append ".json" to file name if file name does not end with ".json"

--- src/main/kotlin/com/github/kuro46/embedscript/script/ScriptExporter.kt
+++ src/main/kotlin/com/github/kuro46/embedscript/script/ScriptExporter.kt
@@ -4,27 +4,42 @@ import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
 
-class ScriptExporter(private val dataFolder: Path, private val scriptManager: ScriptManager) {
-    fun export(world: String, fileName: String): String {
-        val exportTo = ScriptManager.load(dataFolder.resolve(fileName))
+class ScriptExporter(dataFolder: Path, private val scriptManager: ScriptManager) {
+    private val dataFolder: Path = dataFolder.resolve("export")
+
+    init {
+        if (Files.notExists(this.dataFolder)) {
+            Files.createDirectory(this.dataFolder)
+        }
+    }
+
+    fun export(world: String, filePath: Path) {
+        val exportTo = ScriptManager.load(filePath)
 
         for ((position, script) in scriptManager.entries()) {
             if (position.world == world) {
                 exportTo.put(position, script)
             }
         }
-
-        return fileName
     }
 
-    fun import(fileName: String) {
-        val filePath = dataFolder.resolve(fileName)
+    fun resolveByExportFolder(other: String): Path {
+        return dataFolder.resolve(other)
+    }
+
+    fun import(filePath: Path) {
         if (Files.notExists(filePath)) {
-            throw IOException("File: '$fileName' not exists!")
+            throw IOException("'$filePath' not exists!")
         }
         val importFrom = ScriptManager.load(filePath)
         for ((position, script) in importFrom.entries()) {
             scriptManager.put(position, script)
+        }
+    }
+
+    companion object {
+        fun appendJsonExtensionIfNeeded(target: String): String {
+            return if (target.endsWith(".json", ignoreCase = true)) target else "$target.json"
         }
     }
 }
