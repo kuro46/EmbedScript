980cb3ca50ebabea6fae1ace31cdd7205ceea085
shirokuro <46kuro212@gmail.com>
2019-04-20 10:27:34 +0900

    refactor: make Prefix.kt object

--- src/main/kotlin/com/github/kuro46/embedscript/Prefix.kt
+++ src/main/kotlin/com/github/kuro46/embedscript/Prefix.kt
@@ -5,17 +5,10 @@ import org.bukkit.ChatColor
 /**
  * @author shirokuro
  */
-class Prefix private constructor() {
-
-    init {
-        throw UnsupportedOperationException("Constant class.")
-    }
-
-    companion object {
-        val PREFIX = (ChatColor.GRAY.toString() + "<"
-                + ChatColor.DARK_GREEN + "E" + ChatColor.GREEN + "S" + ChatColor.GRAY + "> ")
-        val WARN_PREFIX = PREFIX + ChatColor.GOLD
-        val ERROR_PREFIX = PREFIX + ChatColor.RED
-        val SUCCESS_PREFIX = PREFIX + ChatColor.GREEN
-    }
+object Prefix {
+    val PREFIX = (ChatColor.GRAY.toString() + "<"
+            + ChatColor.DARK_GREEN + "E" + ChatColor.GREEN + "S" + ChatColor.GRAY + "> ")
+    val WARN_PREFIX = PREFIX + ChatColor.GOLD
+    val ERROR_PREFIX = PREFIX + ChatColor.RED
+    val SUCCESS_PREFIX = PREFIX + ChatColor.GREEN
 }
