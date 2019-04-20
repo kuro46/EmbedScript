f4cf48aec25cc29f1bf12f9073134caf6654dc5d
shirokuro <46kuro212@gmail.com>
2019-04-20 12:35:04 +0900

    perf: use current thread when  current thread is Scheduler thread

--- src/main/kotlin/com/github/kuro46/embedscript/util/Scheduler.kt
+++ src/main/kotlin/com/github/kuro46/embedscript/util/Scheduler.kt
@@ -1,20 +1,39 @@
 package com.github.kuro46.embedscript.util
 
+import com.google.common.util.concurrent.Futures
+import java.util.Collections
+import java.util.WeakHashMap
 import java.util.concurrent.Executors
+import java.util.concurrent.Future
 
 /**
  * @author shirokuro
  */
 object Scheduler {
-    private val EXECUTOR = Executors.newCachedThreadPool { r ->
+    private val threads: MutableSet<Thread> = Collections.synchronizedSet(Collections.newSetFromMap(WeakHashMap()))
+    private val executor = Executors.newCachedThreadPool { r ->
         val thread = Thread(r)
         thread.name = "EmbedScript Scheduler Thread"
         thread.isDaemon = true
 
+        threads.add(thread)
+
         thread
     }
 
-    fun execute(task: () -> Unit) {
-        EXECUTOR.execute(task)
+    fun execute(forceOtherThread: Boolean = false, task: () -> Unit) {
+        if (!forceOtherThread && threads.contains(Thread.currentThread())) {
+            task()
+        } else {
+            executor.execute(task)
+        }
+    }
+
+    fun submit(forceOtherThread: Boolean = false, task: () -> Unit): Future<*> {
+        return if (!forceOtherThread && threads.contains(Thread.currentThread())) {
+            Futures.immediateFuture(null)
+        } else {
+            executor.submit(task)
+        }
     }
 }
