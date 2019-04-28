726abcad8e0252534c226335b956966a58dfcfb9
shirokuro <46kuro212@gmail.com>
2019-04-28 10:40:02 +0900

    refactor: resolve some detekt's warnings in MoveListener.kt

--- src/main/kotlin/com/github/kuro46/embedscript/listener/MoveListener.kt
+++ src/main/kotlin/com/github/kuro46/embedscript/listener/MoveListener.kt
@@ -14,6 +14,10 @@ import org.bukkit.event.player.PlayerMoveEvent
  * @author shirokuro
  */
 class MoveListener(embedScript: EmbedScript) : Listener {
+    companion object {
+        private const val GROUND_ACCEPTABLE_MAXIMUM_RANGE = 0.2
+    }
+
     private val scriptProcessor = embedScript.scriptProcessor
     private val scriptManager = embedScript.scriptManager
 
@@ -64,7 +68,9 @@ class MoveListener(embedScript: EmbedScript) : Listener {
         //Expects non-air
         val downerSurface = upperSurface.getRelative(BlockFace.DOWN)
 
-        return upperSurface.type == Material.AIR && downerSurface.type != Material.AIR && to.y - upperSurface.y <= 0.2
+        return upperSurface.type == Material.AIR &&
+                downerSurface.type != Material.AIR &&
+                to.y - upperSurface.y <= GROUND_ACCEPTABLE_MAXIMUM_RANGE
     }
 
     private fun equalsBlock(location: Location, location1: Location): Boolean {
